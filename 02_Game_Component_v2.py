e1a778e1dbc8f66a12136227cb4efa30e34c564a
browngojo <100536433+browngojo@users.noreply.github.com>
2025-02-21 11:43:08 +1300

    Integrated coplour retrieval

--- 02_Game_Component_v2.py
+++ 02_Game_Component_v2.py
@@ -1,10 +1,22 @@
 from tkinter import *
 import csv
+import random
 from functools import partial # To prevent unwanted windows
 # import all_constants as c
 
 
-def get colours():
+def round_ans(val):
+    """
+    Rounds numbers to nearest integer
+    :param val: number to be rounded.
+    :return: Rounded number (an integer)
+    """
+
+    var_rounded = (val * 2 + 1) // 2
+    raw_rounded = "{:.0f}".format(var_rounded)
+    return int(raw_rounded)
+
+def get_colours():
     """
     Retrieves colours from csv file
     :return: list of colours which where each list item has the
@@ -20,7 +32,34 @@ def get colours():
 
     return all_colors
 
+def get_round_colours(val):
+    """
+    Choose four colours from larger list ensuring that the scores are all different.
+    :return: List of colours and score to beat (median of scores)
+    """
 
+    all_colours_list = get_colours()
+
+    round_colours = []
+    colour_scores = []
+
+    # loop until we have four colours with different scores...
+    while len(round_colours) < 4:
+        potential_color = random.choice(all_colours_list)
+
+        # Get the score and check it's not a duplicate
+        if potential_color[1] not in colour_scores:
+            round_colours.append(potential_color)
+            colour_scores.append(potential_color[1])
+
+    # change scores to integers
+    int_scores = [int(x) for x in colour_scores]
+    int_scores.sort()
+
+    median = (int_scores[1] + int_scores[2]) / 2
+    median = round_ans(median)
+
+    return round_colours, median
 
 class StartGame:
     """
